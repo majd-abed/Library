b6e918c6a83554eca5c9377e3612a2245aa422f4
majdcx <majdcx@hotmail.com>
2022-07-10 22:20:12 +0300

    fix darkmode switch

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,11 +3,13 @@ import { TbApps } from "react-icons/tb";
 
 const Dashboard = () => {
   return (
+    <div className="w-32 flex justify-end">
     <div className='flex items-center bg-gray-100 rounded-full pl-2 py-1 max-w-[2.6rem] hover:max-w-[10rem] duration-300 overflow-hidden cursor-pointer'>
       <div className='w-7 h-7 '>
         <TbApps className='w-7 h-7'/>
       </div>
       <div className='w-24 pl-2 font-semibold'>Dashboard</div>
+    </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Logo, Container, Dashboard ,DarkMode} from "./";
+import { Logo, Container, Dashboard, DarkMode } from "./";
 const Navbar = () => {
   return (
-    <main className="shadow-sm border-b dark:border-slate-500 dark:bg-slate-800">
+    <main className='shadow-sm border-b dark:border-slate-500 dark:bg-slate-800'>
       <Container>
-        <div className='flex justify-between items-center px-5'>
-          <div className='flex justify-between items-center w-1/2'>
+        <div className='flex justify-between items-center px-5 relative'>
+          <div className='bg-white dark:bg-slate-800 dark:border-slate-500 absolute -bottom-[1.9rem] right-[1.65rem] border-2 border-t-0 rounded-b-full'>
+            <DarkMode />
+          </div>
+          <div className='flex justify-between items-center flex-1'>
             <Logo />
-          <DarkMode/>
           </div>
           <Dashboard />
         </div>
