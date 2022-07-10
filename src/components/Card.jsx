516bd529a8319e0c1bcb676485bd8efd4b728169
Majd Abed <majdcx@hotmail.com>
2022-07-10 20:49:10 +0300

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,29 +15,29 @@ const Card = ({ cover, title, author, year, description, likes }) => {
 
   return (
     <div className='shadow border hover:shadow-md duration-150 '>
-      <div className='m-auto w-fit py-4'>
+      <div className='m-auto w-fit py-4 smx:pt-2 smx:px-2'>
         <img src={cover} alt='Cover' width={200} height={250} />
       </div>
-      <div className='grid grid-cols-2'>
-        <h2 className='text-lg text-center font-bold col-span-2 py-2'>{title}</h2>
-        <h3 className='font-semibold pl-2 pb-2'>{author}</h3>
-        <h4 className='text-end text-sm pr-2 pb-2'>{year}</h4>
-        <p className='text-sm text-gray-700 col-span-2 px-2 smx:mb-2 h-20'>
+      <div className='grid grid-cols-4'>
+        <h2 className='text-lg text-center font-bold col-span-4 py-2'>{title}</h2>
+        <h3 className='font-semibold col-span-2 pl-2 pb-2 smx:text-sm'>{author}</h3>
+        <h4 className='text-end text-sm col-span-2 pr-2 pb-2'>{year}</h4>
+        <p className='text-sm text-gray-700 col-span-4 px-2 smx:mb-3 h-20'>
           {description}
         </p>
-        <div className='flex justify-evenly items-center pr-8 py-3 bg-gray-100'>
+        <div className='flex justify-evenly col-span-2 items-center sm:pr-8 py-3 bg-gray-100'>
           <MdThumbUp
             className='w-6 h-6 hover:text-blue-500 duration-150 cursor-pointer'
             onClick={modLikes}
-            style={like !== likes ? { color: "#6c6cf2" } : ""}
+            style={like !== likes ? { color: "rgb(59 130 246)" } : ""}
           />
           <MdThumbDown
             className='w-6 h-6 hover:text-red-700 duration-150 cursor-pointer'
             onClick={modDislikes}
-            style={dislike ? { color: "#d52424" } : ""}
+            style={dislike ? { color: "rgb(185 28 28)" } : ""}
           />
         </div>
-        <div className='text-end pr-4 py-3 bg-slate-100 text-blue-800 '>
+        <div className='text-end pr-2 py-3 col-span-2 bg-slate-100 text-blue-800 '>
           {like !== 0 ? (like === 1 ? `${like} Like` : `${like} Likes`) : "No likes"}
         </div>
       </div>
